23d621de0f96c3a6c8aaca6b127bc6b79252f67a
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-11-02 10:22:51 -0400

    Finished tutorial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+    include Visible  
+    has_many :comments, dependent: :destroy
+
     validates :title, presence: true, length: { minimum: 5 }
     validates :body, presence: true, length: { minimum: 10 }
   end
